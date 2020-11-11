990787c92a6fd88fb3c133ba79634b3767b60c7c
RafaelMerchan <dcrafa1999@gmail.com>
2020-11-11 18:24:49 -0500

    Tema 1

---
+++ src/Postres/Postre.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Postres;
+
+import Adicionales.Aderezo;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Rafael
+ */
+public class Postre {
+    String sabor;
+    double precioParcial;
+    ArrayList<Aderezo> aderezos;
+    
+    public Postre(String sabor){
+        aderezos= new ArrayList<>();
+        this.sabor=sabor;
+    }
+    
+    public double calcularPrecioFinal(){
+        double precioFinal;
+        precioFinal=(precioParcial+(precioParcial*0.12))+(aderezos.size()*0.50);
+        return precioFinal;
+    }
+    public String showPrecioFinal(){
+        return "Precio Final: $ " + calcularPrecioFinal();
+    }
+}
